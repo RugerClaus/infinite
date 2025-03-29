c830bbdf4d4bd624b16f741e92d80ebec2eef29f
rugerclaus <rogerrock12games@gmail.com>
2025-03-28 20:53:36 -0600

    updated shit

--- constants.py
+++ constants.py
@@ -1,7 +1,5 @@
 import pygame
 
-pygame.init()
-
 pygame.font.init()
 
 score = 0
@@ -13,12 +11,16 @@ player_y = 215
 player_y_velocity = 0
 player_gravity = 0
 screen = pygame.display.set_mode((800,400))
+tryagaincount = 0
+pausebuttonsize = (100,50)
 
-game_active = True
+game_active = False
+paused = False
 
 clock = pygame.time.Clock()
 
 font = pygame.font.Font('font/Pixeltype.ttf',50)
+pause_font = pygame.font.Font('font/Pixeltype.ttf',30)
 button_font = pygame.font.Font('font/Roboto-Black.ttf',30)
 game_over_font = pygame.font.Font('font/gameover.ttf',50)
 quit_text_x = 70
@@ -34,10 +36,12 @@ tree_1_surface = pygame.image.load('graphics/Tree/tree1.png').convert_alpha()
 tree_2_surface = pygame.image.load('graphics/Tree/tree2.png').convert_alpha()
 tree_1_rect = tree_1_surface.get_rect()
 tree_2_rect = tree_2_surface.get_rect()
-tree_1_rect.x = 2000
-tree_2_rect.x = 2500
+tree_1_rect.x = 1000
+tree_2_rect.x = 1500
 ground_rect = ground_surface.get_rect()
 sky_rect = sky_surface.get_rect()
 snail_rect = snail_surface.get_rect(bottomright = (800,300))
 player_rect = player_surface.get_rect(topleft = (player_x,player_y))
 passed_snail = False
+
+paused_surface = pygame.Surface((800,400))
--- states.py
+++
@@ -1,6 +0,0 @@
-from constants import running
-
-def game_state():
-    return running
-def pause_menu_state():
-    pass
